4cecb82f6a0078b5a87cd2234ff4abd944dbfd10
Berry Burge <bjburge@hotmail.com>
2017-04-12 17:10:52 -0500

    done

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -97,7 +97,6 @@ public class JobForm {
     public void setLocationId(int locationId) {
         this.locationId = locationId;
     }
-
     public int getCoreCompetencieId() {
         return coreCompetencieId;
     }
